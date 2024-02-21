f7c8b3e6c0b9f8221ea2810adcb66845df624809
iwanekaputra <iwanekaputra5@gmail.com>
2024-02-21 19:52:46 +0700

    disable preloader

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,10 +1,10 @@
 @include('layouts.partials.header')
-<!-- Preloader -->
+{{-- <!-- Preloader -->
 <div class="preloader d-flex align-items-center justify-content-center">
     <div class="cssload-container">
         <div class="cssload-loading"><i></i><i></i><i></i><i></i></div>
     </div>
-</div>
+</div> --}}
 
 @include('layouts.partials.navbar')
 
